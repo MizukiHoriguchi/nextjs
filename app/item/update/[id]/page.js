cfc4759c55e93971d96b18efc6b345f8f8c12ea2
MizukiHoriguchi <mhoriguchi@dresscode.co.jp>
2025-01-30 11:56:42 +0900

    URLの修正

--- app/item/update/[id]/page.js
+++ app/item/update/[id]/page.js
@@ -16,7 +16,7 @@ const UpdateItem = (context) => {
     useEffect(() => {
         const getSingleItem = async() => {
             const params = await context.params
-            const response = await fetch(`http://localhost:3000/api/item/readsingle/${params.id}`)
+            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${params.id}`)
             const jsonData = await response.json() 
             const singleItem = jsonData.singleItem
             setTitle(singleItem.title)
